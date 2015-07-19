3a1af21d8bbb45f2a829f2840482419cec3a4f42
yvoloshchuk <yaroslav.voloshchuk@grammarly.com>
2015-07-19 23:44:50 +0300

    test updates for api changes

--- test/unit/AllureReporter.spec.js
+++ test/unit/AllureReporter.spec.js
@@ -59,27 +59,27 @@ describe('AllureReporter', function() {
             reporter.specSuccess('test', new TestResult('a suite', 'first', null, true, false));
             expect(allure.startSuite).toHaveBeenCalled();
             expect(allure.startCase).toHaveBeenCalled();
-            expect(allure.endCase).toHaveBeenCalledWith('[test] a suite', 'first', 'passed', undefined, jasmine.any(Number));
+            expect(allure.endCase).toHaveBeenCalledWith('passed', undefined, jasmine.any(Number));
             expect(allure.endSuite).not.toHaveBeenCalled();
 
             allure.startSuite.calls.reset();
             reporter.specSuccess('test', new TestResult('a suite', 'second', null, true, false));
-            expect(allure.endCase).toHaveBeenCalledWith('[test] a suite', 'second', 'passed', undefined, jasmine.any(Number));
+            expect(allure.endCase).toHaveBeenCalledWith('passed', undefined, jasmine.any(Number));
             expect(allure.startSuite).not.toHaveBeenCalled();
         });
 
         it("should report failed tests", function () {
             reporter.specFailure('test', new TestResult('fails', 'first', 'Expected but not happened', false, false));
-            expect(allure.endCase).toHaveBeenCalledWith('[test] fails', 'first', 'failed', jasmine.any(Object), jasmine.any(Number));
+            expect(allure.endCase).toHaveBeenCalledWith('failed', jasmine.any(Object), jasmine.any(Number));
 
             reporter.specFailure('test', new TestResult('fails', 'second', 'Error without reason', false, false));
-            expect(allure.endCase).toHaveBeenCalledWith('[test] fails', 'second', 'broken', jasmine.any(Object), jasmine.any(Number));
+            expect(allure.endCase).toHaveBeenCalledWith('broken', jasmine.any(Object), jasmine.any(Number));
         });
 
         it("should report skipped tests", function () {
             reporter.specSkipped('test', new TestResult('future', 'ignored test', null, false, true));
-            expect(allure.startCase).toHaveBeenCalledWith('[test] future', 'ignored test', jasmine.any(Number));
-            expect(allure.endCase).toHaveBeenCalledWith('[test] future', 'ignored test', 'pending', jasmine.any(Object), jasmine.any(Number));
+            expect(allure.startCase).toHaveBeenCalledWith('ignored test', jasmine.any(Number));
+            expect(allure.endCase).toHaveBeenCalledWith('pending', jasmine.any(Object), jasmine.any(Number));
         });
 
         it("should finish suites on testrun finish", function () {
@@ -88,8 +88,8 @@ describe('AllureReporter', function() {
                 'b': [{start: 45, stop: 48}, {start: 51, stop: 57}]
             };
             reporter.onRunComplete();
-            expect(allure.endSuite).toHaveBeenCalledWith('a', 7);
-            expect(allure.endSuite).toHaveBeenCalledWith('b', 57);
+            expect(allure.endSuite).toHaveBeenCalledWith(7);
+            expect(allure.endSuite).toHaveBeenCalledWith(57);
         });
     });
 
